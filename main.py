bf869b35da324a588cd6b298c167d0770328f70b
Roman Vihristiuk <45902869+ramzes667@users.noreply.github.com>
2021-03-27 13:28:18 +0300

    interface pokemons

--- main.py
+++ main.py
@@ -1,3 +1,43 @@
 import streamlit as st
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
+import os
+import cv2
+import random
+from PIL import Image
+#st.title('NFT-Pokemons')
+
+global path
+path = 'pokemon-gpt-2-output'
+im_list = os.listdir(path)
+random_img = random.randint(1, len(im_list))
+
+st.set_page_config(layout="wide")
+
+st.markdown("""
+<style>
+.big-font {
+    font-size:100px !important;
+}
+.small-font {
+    font-size: 50px !important;
+
+}
+</style>
+""", unsafe_allow_html=True)
+
+st.markdown('<p class="big-font">NFT-Pokemons</p>', unsafe_allow_html=True)
+
+st.markdown('<p class="small-font">Generate your own image</p>', unsafe_allow_html=True)
+
+def gen_file(size, random_img):
+    img = Image.open(path + '/' + im_list[random_img])
+    resized_im = img.resize((size, size))
+    st.image(resized_im)
+
+# st.write("""
+# # Generate your own image
+# """)
+
+size = st.sidebar.slider('size', 64, 640)
+open_file = st.sidebar.button('GENERATE')
+if open_file:
+    gen_file(size, random_img)
